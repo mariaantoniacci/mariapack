f4c895bfffc425354762521624c7306e22f9efd7
Maria Antoniacci <mariaantoniacci19@gmail.com>
2024-12-18 14:31:00 +0100

    Create simplestuff.R

---
+++ R/simplestuff.R
@@ -0,0 +1,23 @@
+# function to build the function is function
+
+# use graph parenthesis to open and close the function 
+# using x and y gives back the element z
+function(x,y) {
+  z=x+y
+return(z)
+  }
+
+# i'll assing this to an object
+simplestuff <- function(x,y) {
+  z=x+y
+return(z)
+  }
+
+# try it
+simplestuff(1,2)
+
+# or i'll assign to obj the elements
+a <- 1
+b <- 2
+
+simplestuff(a,b)
